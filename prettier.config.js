49b2ddc6646cfa2eed068d70311150eaccf0d5fe
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-22 15:20:09 +0900

    start gdg on campus yonsei web project

---
+++ prettier.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  jsxSingleQuote: false,
+  printWidth: 120,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+}
