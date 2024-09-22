373ff9dfa029b462008d4b4521cc411b665133e2
jhyunwoo <jhyunwoo0228@gmail.com>
2024-09-22 16:00:26 +0900

    add auth.js and prisma with postgres

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/lib/prisma'
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
