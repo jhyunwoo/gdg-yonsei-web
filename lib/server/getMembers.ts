cb2decf4fa47dfa91e29fe168992190ac8a0048b
jhyunwoo <jhyunwoo0228@gmail.com>
2024-10-08 13:35:10 +0900

    add filter on member api

--- lib/server/getMembers.ts
+++ lib/server/getMembers.ts
@@ -1,14 +1,17 @@
 import { prisma } from '@/lib/prisma'
+import { $Enums } from '@prisma'
+import Role = $Enums.Role
 
 /**
  * 멤버 리스트를 가져오는 함수
  */
-export default async function getMembers() {
+export default async function getMembers({ stage, part, role }: { stage?: string; part?: string; role?: Role }) {
   return prisma.user.findMany({
     where: {
       NOT: {
         role: 'USER',
       },
+      AND: [stage ? { stage } : {}, part ? { part } : {}, role ? { role } : {}],
     },
   })
 }
