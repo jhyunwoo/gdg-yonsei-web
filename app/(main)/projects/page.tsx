0fe1ecc0817c91ab5d56d76db20de0c58fcfcfac
junhyun bang <flintintoe@gmail.com>
2024-10-29 12:48:11 +0900

    Chore: Refactor previous commits to new project-page branch (from new main branch)

--- app/(main)/projects/page.tsx
+++ app/(main)/projects/page.tsx
@@ -1,7 +1,42 @@
-export default function ProjectsPage() {
+import Link from 'next/link'
+import { FC } from 'react'
+import Header from '../../components/header'
+
+const ProjectsPage: FC = () => {
   return (
-    <div>
-      <div>Project Page</div>
-    </div>
-  );
+    <body>
+      <Header></Header>
+      <section className={'min-h-[400px] m-2'}>
+        <div className='w-[70%] max-w-[1300px] mx-auto my-20 flex-col'>
+          <div className={''}>
+            <article className={''}>
+              <h1 className={'text-4xl font-bold'}>Projects</h1>
+              <hr></hr>
+              <div className={'flex gap-2'}>
+                <div>
+                  Stage
+                </div>
+                <div className="rounded-md border border-slate-300 py-0.5 px-2.5 text-center text-sm transition-all shadow-sm text-slate-600">
+                22-23
+                </div>
+                <div className="rounded-md border border-slate-300 py-0.5 px-2.5 text-center text-sm transition-all shadow-sm text-slate-600">
+                23-24
+                </div>
+                <div className="rounded-md border border-slate-300 py-0.5 px-2.5 text-center text-sm transition-all shadow-sm text-slate-600">
+                24-25
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce blandit eros vitae quam facilisis facilisis. Praesent porttitor purus nec ante fringilla malesuada. Morbi sollicitudin eleifend interdum. Etiam sed quam a leo condimentum lacinia. Duis aliquet vestibulum convallis. Suspendisse scelerisque malesuada lectus quis mattis. Nullam vel ullamcorper mi. Etiam maximus accumsan ornare. Integer eu laoreet felis, at sollicitudin turpis. Ut hendrerit convallis iaculis.
+
+              Donec efficitur libero sit amet interdum mollis. Donec pretium gravida massa, non semper ante. Vestibulum mollis sit amet leo at vestibulum. Nullam tempor metus sed lacus interdum eleifend. Integer sit amet leo consectetur, dictum purus a, porttitor eros. Duis laoreet rutrum nisi, vitae tempor dui ultricies imperdiet. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nunc nisl lectus, malesuada quis volutpat sed, tristique eget ipsum. Nunc vulputate quam non turpis ultricies cursus. Vestibulum dapibus est a ex aliquet, tincidunt facilisis odio euismod. Pellentesque finibus metus quis velit lacinia, id fermentum nulla pulvinar. Donec placerat ac eros in porttitor. Maecenas interdum, velit vel consequat dictum, diam urna rhoncus dolor, quis facilisis erat metus sed elit. Etiam porttitor elementum aliquet. Nunc venenatis ut enim at ultricies. Sed porta a arcu interdum pharetra.
+
+              Sed pulvinar turpis orci, nec ultrices purus porttitor nec. Praesent id lacus ut sem varius rutrum. Cras condimentum iaculis nisi, in aliquet orci fermentum in. Maecenas iaculis pharetra nulla vel malesuada. Donec ut ex neque. Duis quis massa aliquet, ullamcorper augue in, ultrices arcu. Donec fermentum lobortis lorem, a dignissim mauris mattis id. In rutrum augue a mauris porta molestie. Vivamus luctus ante velit, vitae pellentesque diam vestibulum tempus.</p>
+            </article>
+          </div>
+        </div>
+      </section>
+    </body>
+  )
 }
+
+export default ProjectsPage
