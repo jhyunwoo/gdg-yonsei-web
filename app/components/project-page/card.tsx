674e9a51971661e58c61dbdc916d917310746761
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-05 19:41:22 +0900

    add type on data

--- app/components/project-page/card.tsx
+++ app/components/project-page/card.tsx
@@ -1,27 +1,27 @@
-import NoImg from "@/app/components/placeholder-image"
+import NoImg from "@/app/components/placeholder-image";
 
 // components/ProjectCard.tsx
 
-import React from 'react';
+import { FC } from "react";
 
 interface ProjectCardProps {
-    projectName: string;
-    description: string;
+  projectName: string;
+  description: string;
 }
 
 // Create the functional component using props
-const ProjectCard: React.FC<ProjectCardProps> = ({ projectName, description }) => {
-    return (
-        <article className="rounded-md border border-slate-400 p-2 flex flex-row gap-4 max-h-[240px] m-4">
-            <div className="min-h-[160px] min-w-[320px] overflow-x-hidden bg-clip-border">
-                <NoImg />
-            </div>
-            <div>
-                <h1 className={"text-2xl"}>{projectName}</h1>
-                <p>{description}</p>
-            </div>
-        </article>
-    );
+const ProjectCard: FC<ProjectCardProps> = ({ projectName, description }) => {
+  return (
+    <article className="rounded-md border border-slate-400 p-2 flex flex-row gap-4 max-h-[240px] m-4">
+      <div className="min-h-[160px] min-w-[320px] overflow-x-hidden bg-clip-border">
+        <NoImg />
+      </div>
+      <div>
+        <h1 className={"text-2xl"}>{projectName}</h1>
+        <p>{description}</p>
+      </div>
+    </article>
+  );
 };
 
 export default ProjectCard;
