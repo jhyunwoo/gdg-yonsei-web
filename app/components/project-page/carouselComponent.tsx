dfd6e8e51693b96e8bfc3960721bfe0d51fd3433
junhyun bang <flintintoe@gmail.com>
2024-11-05 09:45:32 +0900

    fix: Create carousel beta

--- app/components/project-page/carouselComponent.tsx
+++ app/components/project-page/carouselComponent.tsx
@@ -1,31 +1,40 @@
+'use client';
+
 // components/Carousel.js
-import React, { useCallback } from 'react';
-import useEmblaCarousel from 'embla-carousel';
+import React, { useState } from 'react';
+import '@/app/css/carousel.css'
 
-const CarouselComponent = () => {
-//   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+interface CarouselProps {
+  images: string[];
+}
 
-//   const scrollPrev = useCallback(() => {
-//     if (emblaApi) emblaApi.scrollPrev();
-//   }, [emblaApi]);
+const Carousel: React.FC<CarouselProps> = ({ images }) => {
+  const [currentIndex, setCurrentIndex] = useState(0);
 
-//   const scrollNext = useCallback(() => {
-//     if (emblaApi) emblaApi.scrollNext();
-//   }, [emblaApi]);
+  const prevImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : images.length - 1));
+  };
+
+  const nextImage = () => {
+    setCurrentIndex((prevIndex) => (prevIndex < images.length - 1 ? prevIndex + 1 : 0));
+  };
 
   return (
-    <div>
-        {/* <Carousel>
-            <CarouselContent>
-                <CarouselItem>...</CarouselItem>
-                <CarouselItem>...</CarouselItem>
-                <CarouselItem>...</CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-        </Carousel> */}
+    <div className="carousel">
+      <button onClick={prevImage} className="carousel-button">◀</button>
+      <div className="carousel-images">
+        {images.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt={`Slide ${index}`}
+            className={`carousel-image ${index === currentIndex ? 'active' : ''}`}
+          />
+        ))}
+      </div>
+      <button onClick={nextImage} className="carousel-button">▶</button>
     </div>
   );
 };
 
-export default CarouselComponent;
+export default Carousel;
