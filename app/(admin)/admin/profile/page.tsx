79f44c7645418109ed55f1da4a70d2a2a53730cf
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-08 14:01:01 +0900

    test passkey auth 2

--- app/(admin)/admin/profile/page.tsx
+++ app/(admin)/admin/profile/page.tsx
@@ -1,9 +1,14 @@
 import AdminPageLayout from "@/app/components/admin-page-layout";
+import AuthProvider from "@/app/(admin)/auth/sign-in/auth-provider";
+import PasskeyButton from "@/app/(admin)/auth/sign-in/passkey-button";
 
 export default async function ProfilePage() {
   return (
     <AdminPageLayout>
       <div>Profile Page</div>
+      <AuthProvider>
+        <PasskeyButton />
+      </AuthProvider>
     </AdminPageLayout>
   );
 }
