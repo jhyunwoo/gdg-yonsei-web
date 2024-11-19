12077e374a08937e95e895602cb8a14fdd9e8d45
jhyunwoo <jhyunwoo0228@gmail.com>
2024-11-19 13:04:59 +0900

    fix session ui

--- app/(main)/sessions/[sessionId]/session.tsx
+++ app/(main)/sessions/[sessionId]/session.tsx
@@ -1,13 +1,14 @@
 import getSession from "@/lib/server/get-session";
 import Image from "next/image";
+import formatDateYYMMDD from "@/lib/format-date-yymmdd";
 
 export default async function Session({ id }: { id: string }) {
   const session = await getSession(id);
   return (
-    <div className={"w-full p-4"}>
-      <div>
-        <div className={"text-lg font-bold"}>{session.title}</div>
-        <div></div>
+    <div className={"w-full p-4 flex flex-col items-start"}>
+      <div className={"flex flex-col items-end"}>
+        <div className={"text-2xl font-bold"}>{session.title}</div>
+        <div>{formatDateYYMMDD(session.date)}</div>
       </div>
       <Image
         src={`https://image.gdgyonsei.moveto.kr/sessions/${session.id}/${session.defaultImage}`}
@@ -16,6 +17,11 @@ export default async function Session({ id }: { id: string }) {
         height={300}
         className={"w-full"}
       />
+      <div>
+        {session.description?.map((data, index) => (
+          <div key={index}>{data}</div>
+        ))}
+      </div>
     </div>
   );
 }
--- app/(main)/sessions/page.tsx
+++ app/(main)/sessions/page.tsx
@@ -2,13 +2,7 @@ import getSessions from "@/lib/server/get-sessions";
 import Image from "next/image";
 import Link from "next/link";
 import MainLayout from "@/app/components/main-layout";
-
-function formatDate(date: Date) {
-  const year = date.getFullYear().toString().slice(2); // 마지막 두 자리를 가져옵니다.
-  const month = (date.getMonth() + 1).toString().padStart(2, "0"); // 월을 2자리로 만듭니다.
-  const day = date.getDate().toString().padStart(2, "0"); // 일을 2자리로 만듭니다.
-  return `${year}.${month}.${day}`;
-}
+import formatDateYYMMDD from "@/lib/format-date-yymmdd";
 
 export default async function SessionsPage() {
   const sessions = await getSessions();
@@ -43,7 +37,7 @@ export default async function SessionsPage() {
               <div />
             )}
             <div className={"text-3xl font-semibold"}>
-              {formatDate(session.date)}
+              {formatDateYYMMDD(session.date)}
             </div>
           </Link>
         ))}
